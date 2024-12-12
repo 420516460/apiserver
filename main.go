4f548b1613b6415c8521765195705576a377d899
AlfredYong <yzbsedate@gmail.com>
2024-12-12 21:33:12 +0800

    use viper to set config

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"apiserver/config"
 	"apiserver/router"
 	"errors"
 	_ "fmt"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 	"log"
 	"net/http"
 	"time"
@@ -11,26 +14,38 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
+var (
+	cfg = pflag.StringP("config", "c", "", "apiserver config file path")
+) //TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 
 func main() {
+	pflag.Parse()
+
+	// init config
+	if err := config.Init(*cfg); err != nil {
+		panic(err)
+	}
+
+	// Set gin mode
+	gin.SetMode(viper.GetString("runmode"))
+
 	// Create the Gin engine.
 	g := gin.New()
 
 	// gin middlewares
 	middlewares := []gin.HandlerFunc{}
 
-	// Routes.
+	// Routes, set Http Header
 	router.Load(
-		// Cores.
+		// Cores
 		g,
 
-		// Middlewares.
+		// Middlewares
 		middlewares...,
 	)
 
-	// Ping the server to make sure the router is working.
+	// Ping the server to make sure the router is working
 	go func() {
 		if err := pingServer(); err != nil {
 			log.Fatal("The router has no response, or it might took too long to start up", err)
@@ -38,14 +53,14 @@ func main() {
 		log.Print("The router has been deployed successfully")
 	}()
 
-	log.Printf("Start to listening the incoming requests on http address: %s", ":8080")
-	log.Printf(http.ListenAndServe(":8080", g).Error())
+	log.Printf("Start to listening the incoming requests on http address: %s", viper.GetString("addr"))
+	log.Printf(http.ListenAndServe(viper.GetString("addr"), g).Error())
 }
 
 func pingServer() error {
-	for i := 0; i < 10; i++ {
+	for i := 0; i < viper.GetInt("max_ping_count"); i++ {
 		// Ping the server by sending a GET request to '/health'
-		resp, err := http.Get("http://127.0.0.1:8080" + "/sd/health")
+		resp, err := http.Get(viper.GetString("url") + "/sd/health")
 		if err == nil && resp.StatusCode == 200 {
 			return nil
 		}
