97fbe672c518e07ece44209fe39332ecb3273e7b
AlfredYong <yzbsedate@gmail.com>
2024-12-12 12:13:21 +0800

    fix pingServer

--- main.go
+++ main.go
@@ -30,9 +30,6 @@ func main() {
 		middlewares...,
 	)
 
-	log.Printf("Start to listening the incoming requests on http address: %s", ":8080")
-	log.Printf(http.ListenAndServe(":8080", g).Error())
-
 	// Ping the server to make sure the router is working.
 	go func() {
 		if err := pingServer(); err != nil {
@@ -40,18 +37,21 @@ func main() {
 		}
 		log.Print("The router has been deployed successfully")
 	}()
+
+	log.Printf("Start to listening the incoming requests on http address: %s", ":8080")
+	log.Printf(http.ListenAndServe(":8080", g).Error())
 }
 
 func pingServer() error {
 	for i := 0; i < 10; i++ {
 		// Ping the server by sending a GET request to '/health'
-		resp, err := http.Get("http://localhost:8080/ping" + "/sd/health")
+		resp, err := http.Get("http://127.0.0.1:8080" + "/sd/health")
 		if err == nil && resp.StatusCode == 200 {
 			return nil
 		}
 
-		// Sleep for a sec to continue the next ping.
-		log.Print("Waiting for the server to start")
+		// Sleep for a second to continue the next ping.
+		log.Print("Waiting for the router, retry in 1 second.")
 		time.Sleep(time.Second)
 	}
 	return errors.New("cannot connect to the router")
